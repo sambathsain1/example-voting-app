e611552c64050d8d0e1172d036766c37a27da8ef
karthigakandasamy <karthiga.kandasamy@globallogic.com>
2025-11-18 13:27:04 +0530

    Update SampleTest.java

--- worker/src/test/java/com/example/SampleTest.java
+++ worker/src/test/java/com/example/SampleTest.java
@@ -1,14 +1,14 @@
 package com.example;
 
 import io.qameta.allure.Description;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
-class SampleTest {
+public class SampleTest {
 
     @Test
     @Description("Verify basic functionality")
-    void verifyTrueCondition() {
-        assertTrue(true);
+    public void verifyTrueCondition() {
+        Assert.assertTrue(true);  // TestNG assert
     }
 }
